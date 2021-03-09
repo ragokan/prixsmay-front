53573b2ebe147aacd3cc364da77ffa71cb301a78
ragokan <okanyldrmop@gmail.com>
2021-03-09 22:06:49 +0300

    update picture loading

--- src/pages/user/updatePicture.tsx
+++ src/pages/user/updatePicture.tsx
@@ -15,7 +15,7 @@ import {
 } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { BsImageFill } from "react-icons/bs";
 import { UpdateProfilePictureAction } from "../../actions/ProfileActions";
 import { DefaultProfilePictureSelector } from "../../components/form/DefaultProfilePictureSelector";
@@ -23,14 +23,17 @@ import { Container } from "../../components/utility/Container";
 
 const UpdateProfilePicture: NextPage = () => {
   const inputRef = useRef<HTMLInputElement>();
-  const [profilePicture, setProfilePicture] = React.useState<string>("");
-  const [file, setFile] = React.useState<File>(null);
+  const [profilePicture, setProfilePicture] = useState<string>("");
+  const [file, setFile] = useState<File>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const router = useRouter();
   const submitForm = async () => {
+    setIsLoading(true);
     const callback = () => router.push("/user/account");
     file
       ? await UpdateProfilePictureAction({ image: file, callback })
       : await UpdateProfilePictureAction({ pictureUrl: profilePicture, callback });
+    setIsLoading(false);
   };
 
   return (
@@ -50,7 +53,7 @@ const UpdateProfilePicture: NextPage = () => {
                 setProfilePicture={setProfilePicture}
                 hideTexts
               />
-              <Button colorScheme="blue" type="submit" onClick={() => submitForm()}>
+              <Button colorScheme="blue" type="submit" onClick={() => submitForm()} isLoading={isLoading}>
                 Update
               </Button>
             </TabPanel>
@@ -69,7 +72,13 @@ const UpdateProfilePicture: NextPage = () => {
                   <Input value={file ? file.name + " is currently selected." : "Click to upload image!"} readOnly />
                 </InputGroup>
               </FormControl>
-              <Button colorScheme="blue" type="submit" disabled={!file} onClick={() => submitForm()}>
+              <Button
+                colorScheme="blue"
+                type="submit"
+                disabled={!file}
+                onClick={() => submitForm()}
+                isLoading={isLoading}
+              >
                 Update
               </Button>
             </TabPanel>
