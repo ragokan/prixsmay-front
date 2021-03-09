e3460668b33d62a9e1b373629f06051bc034d1ca
ragokan <okanyldrmop@gmail.com>
2021-03-09 22:12:02 +0300

    clear and block code repeat

--- src/actions/ProfileActions.ts
+++ src/actions/ProfileActions.ts
@@ -1,6 +1,7 @@
 import { setUserState } from "../state/UserState";
 import { IUserResponse } from "../types/ResponseTypes";
 import { apios } from "../utils/Apios";
+import { AddAlertAction } from "./AlertActions";
 
 export const GetProfileAction = async (username: string) => {
   const { data } = await apios.get<IUserResponse>(`/profile/${username}`);
@@ -14,8 +15,10 @@ interface ProfileUpdateTypes {
   callback?: Function;
 }
 export const UpdateProfilePictureAction = async ({ image, pictureUrl, callback }: ProfileUpdateTypes) => {
-  const cb = () => callback && callback();
-
+  const cb = () => {
+    callback && callback();
+    AddAlertAction("Profile picture is updated successfully!", "green.400", 3000);
+  };
   if (pictureUrl) {
     const { data } = await apios.post<IUserResponse>("/user/addProfile", { pictureUrl });
     data.user && setUserState({ user: data.user });
--- src/components/main/MainRightPanel.tsx
+++ src/components/main/MainRightPanel.tsx
@@ -1,56 +1,10 @@
-import { Box, Flex, Stack, Text, useColorMode, useMediaQuery } from "@chakra-ui/react";
+import { useColorMode, useMediaQuery } from "@chakra-ui/react";
 import React from "react";
-import { CommunityState } from "../../state/CommunityState";
-import { UpComponent } from "../icons/UpComponent";
-import { BothLink } from "../utility/BothLink";
+import { TopCommunityCard } from "../community/TopCommunityCard";
 
 export const MainRightPanel: React.FC = () => {
-  const communities = CommunityState((state) => state.communities);
-  const topCommunities = communities ? communities.slice(0, 5) : [];
-
   const [isMobile] = useMediaQuery("(max-width: 800px)");
   const { colorMode } = useColorMode();
 
-  return (
-    <>
-      {!isMobile && (
-        <Stack spacing={8} flex={1} width="100%">
-          <Flex
-            p={5}
-            shadow="lg"
-            borderWidth="2px"
-            borderRadius="4px"
-            bg={colorMode === "dark" ? "#151516" : "#F7F9FA"}
-            position="relative"
-            width="100%"
-          >
-            <Box
-              height="24"
-              bg="blue.400"
-              width="100%"
-              position="absolute"
-              top="0"
-              left="0"
-              zIndex={0}
-              borderTopRadius={5}
-            />
-            <Box zIndex={1} width="100%">
-              <Stack marginTop="10">
-                <Text fontWeight="bold">Top Communities To Follow</Text>
-              </Stack>
-              <Stack marginTop="10">
-                {topCommunities.map((community, index) => (
-                  <BothLink href={`/c/${community.name}`} key={community.id}>
-                    <Box cursor="pointer">
-                      {index + 1} <UpComponent /> c/{community.name}
-                    </Box>
-                  </BothLink>
-                ))}
-              </Stack>
-            </Box>
-          </Flex>
-        </Stack>
-      )}
-    </>
-  );
+  return <>{!isMobile && <TopCommunityCard colorMode={colorMode} />}</>;
 };
